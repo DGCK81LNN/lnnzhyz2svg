7b3525d0edad06eb65e612fe119fc016fdc71668
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2023-10-29 16:25:59 +0800

    test: replace chai with earl for assertions

--- test/compile.spec.js
+++ test/compile.spec.js
@@ -1,7 +1,7 @@
-const { expect } = require("chai")
+const { expect } = require("earl")
 const { compileMandarin, compileShidinn } = require("../src/compile")
 
-/** @typedef {import("../src/types").CompiledText} CompiledText */
+const Sub = expect.subset
 
 describe("compile", function () {
   describe("mandarin", function () {
@@ -10,10 +10,31 @@ describe("compile", function () {
         const source = "^zhung1_hua2_y3_z4"
         const result = compileMandarin(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{proper:true,main:{consonant:"zh"},pre:[],post:[{glide:"u",vowel:"e",coda:"ng"}],reverseAffixes:false},{main:{consonant:"h"},pre:[{glide:"u",vowel:"a"}],post:[],reverseAffixes:false},{main:{consonant:""},pre:[{glide:"y"}],post:[],reverseAffixes:true},{main:{consonant:"z"},pre:[],post:[{}],reverseAffixes:true}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({
+              proper: true,
+              main: { consonant: "zh" },
+              pre: [],
+              post: [{ glide: "u", vowel: "e", coda: "ng" }],
+            }),
+            Sub({
+              main: { consonant: "h" },
+              pre: [{ glide: "u", vowel: "a" }],
+              post: [],
+            }),
+            Sub({
+              main: { consonant: "" },
+              pre: [{ glide: "y", reversed: true }],
+              post: [],
+            }),
+            Sub({
+              main: { consonant: "z" },
+              pre: [],
+              post: [{ reversed: true }],
+            }),
+          ],
+        ])
       })
 
       it("handles special spellings and special syllables", function () {
@@ -21,53 +42,132 @@ describe("compile", function () {
           "hm hng   ng1   iung1   eh1   o1 ong1 ou1 uo1 bo1 hong1 hou1"
         const result = compileMandarin(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */
+        expect(result).toEqual([
+          // hm hng
           [
-            [{main:{consonant:"h"},pre:[],post:[{consonant:"m"}]}],
-            [{main:{consonant:"h"},pre:[],post:[{consonant:""}]}],
+            Sub({
+              main: { consonant: "h" },
+              pre: [],
+              post: [{ consonant: "m" }],
+            }),
+          ],
+          [
+            Sub({
+              main: { consonant: "h" },
+              pre: [],
+              post: [{ consonant: "" }],
+            }),
+          ],
 
-            [{main:{consonant:""},pre:[],post:[{}],reverseAffixes:false}],
+          // ng
+          [Sub({ main: { consonant: "" }, pre: [], post: [{}] })],
 
-            [{main:{consonant:""},pre:[],post:[{glide:"y",vowel:"e",coda:"ng"}],reverseAffixes:false}],
+          // iung
+          [
+            Sub({
+              main: { consonant: "" },
+              pre: [],
+              post: [{ glide: "y", vowel: "e", coda: "ng" }],
+            }),
+          ],
 
-            [{main:{consonant:""},pre:[],post:[{vowel:"eh"}],reverseAffixes:false}],
+          //eh
+          [Sub({ main: { consonant: "" }, pre: [], post: [{ vowel: "eh" }] })],
 
-            [{main:{consonant:""},pre:[],post:[{vowel:"o"}],reverseAffixes:false}],
-            [{main:{consonant:""},pre:[],post:[{vowel:"o",coda:"ng"}],reverseAffixes:false}],
-            [{main:{consonant:""},pre:[],post:[{vowel:"e",coda:"u"}],reverseAffixes:false}],
-            [{main:{consonant:""},pre:[],post:[{glide:"u",vowel:"e"}],reverseAffixes:false}],
-            [{main:{consonant:"b"},pre:[],post:[{glide:"u",vowel:"e"}],reverseAffixes:false}],
-            [{main:{consonant:"h"},pre:[],post:[{glide:"u",vowel:"e",coda:"ng"}],reverseAffixes:false}],
-            [{main:{consonant:"h"},pre:[],post:[{vowel:"e",coda:"u"}],reverseAffixes:false}],
-          ]
-        expect(result).to.deep.equal(expected)
+          // o ong ou uo bo hong hou
+          [Sub({ main: { consonant: "" }, pre: [], post: [{ vowel: "o" }] })],
+          [
+            Sub({
+              main: { consonant: "" },
+              pre: [],
+              post: [{ vowel: "o", coda: "ng" }],
+            }),
+          ],
+          [
+            Sub({
+              main: { consonant: "" },
+              pre: [],
+              post: [{ vowel: "e", coda: "u" }],
+            }),
+          ],
+          [
+            Sub({
+              main: { consonant: "" },
+              pre: [],
+              post: [{ glide: "u", vowel: "e" }],
+            }),
+          ],
+          [
+            Sub({
+              main: { consonant: "b" },
+              pre: [],
+              post: [{ glide: "u", vowel: "e" }],
+            }),
+          ],
+          [
+            Sub({
+              main: { consonant: "h" },
+              pre: [],
+              post: [{ glide: "u", vowel: "e", coda: "ng" }],
+            }),
+          ],
+          [
+            Sub({
+              main: { consonant: "h" },
+              pre: [],
+              post: [{ vowel: "e", coda: "u" }],
+            }),
+          ],
+        ])
       })
 
       it("recognises letterals", function () {
         const source = "8_1 ^L_^N_^N Xa"
         const result = compileMandarin(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */
+        expect(result).toEqual([
           [
-            [{main:{consonant:"8"},pre:[],post:[]},{main:{consonant:"1"},pre:[],post:[]}],
-            [{proper:true,main:{consonant:"l"},pre:[],post:[]},{proper:true,main:{consonant:"n"},pre:[],post:[]},{proper:true,main:{consonant:"n"},pre:[],post:[]}],
-            [{main:{consonant:"xa"},pre:[],post:[]}],
-          ]
-        expect(result).to.deep.equal(expected)
+            Sub({ main: { consonant: "8" }, pre: [], post: [] }),
+            Sub({ main: { consonant: "1" }, pre: [], post: [] }),
+          ],
+          [
+            Sub({ proper: true, main: { consonant: "l" }, pre: [], post: [] }),
+            Sub({ proper: true, main: { consonant: "n" }, pre: [], post: [] }),
+            Sub({ proper: true, main: { consonant: "n" }, pre: [], post: [] }),
+          ],
+          [Sub({ main: { consonant: "xa" }, pre: [], post: [] })],
+        ])
       })
 
       it("recognises hyphens", function () {
         const source = "i1_xin1-i1_i4"
         const result = compileMandarin(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{main:{consonant:""},pre:[],post:[{glide:"i"}],reverseAffixes:false},{main:{consonant:"x"},pre:[],post:[{glide:"i",vowel:"e",coda:"n"}],reverseAffixes:false},{hyphen:true,main:{consonant:""},pre:[],post:[{glide:"i"}],reverseAffixes:false},{main:{consonant:""},pre:[],post:[{glide:"i"}],reverseAffixes:true}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({
+              main: { consonant: "" },
+              pre: [],
+              post: [{ glide: "i" }],
+            }),
+            Sub({
+              main: { consonant: "x" },
+              pre: [],
+              post: [{ glide: "i", vowel: "e", coda: "n" }],
+            }),
+            Sub({
+              hyphen: true,
+              main: { consonant: "" },
+              pre: [],
+              post: [{ glide: "i" }],
+            }),
+            Sub({
+              main: { consonant: "" },
+              pre: [],
+              post: [{ glide: "i", reversed: true }],
+            }),
+          ],
+        ])
       })
     })
   })
@@ -78,68 +178,177 @@ describe("compile", function () {
         const source = "Vnu8_AHL ^xdi8_aho"
         const result = compileShidinn(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{pre:[{vowel:"e",coda:"i"}],main:{consonant:"nj"},post:[{glide:"u",vowel:"e",coda:"ng"}]},{pre:[{vowel:"uu"}],main:{consonant:"nz"},post:[{vowel:"a",coda:"u"}]}],[{proper:true,pre:[{consonant:"x"}],main:{consonant:"d"},post:[{glide:"i",vowel:"e",coda:"ng"}]},{pre:[{vowel:"a"}],main:{consonant:"h"},post:[{vowel:"oo"}]}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({
+              pre: [{ vowel: "e", coda: "i" }],
+              main: { consonant: "nj" },
+              post: [{ glide: "u", vowel: "e", coda: "ng" }],
+            }),
+            Sub({
+              pre: [{ vowel: "uu" }],
+              main: { consonant: "nz" },
+              post: [{ vowel: "a", coda: "u" }],
+            }),
+          ],
+          [
+            Sub({
+              proper: true,
+              pre: [{ consonant: "x" }],
+              main: { consonant: "d" },
+              post: [{ glide: "i", vowel: "e", coda: "ng" }],
+            }),
+            Sub({
+              pre: [{ vowel: "a" }],
+              main: { consonant: "h" },
+              post: [{ vowel: "oo" }],
+            }),
+          ],
+        ])
       })
 
       it("compiles a slightly more complex piece of shidinn text", function () {
         const source = "yy8i_xho_kD3H t38_gzoT xd8"
         const result = compileShidinn(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{pre:[{consonant:"y"}],main:{consonant:"y"},post:[{vowel:"e",coda:"ng"},{glide:"i"}]},{pre:[{consonant:"x"}],main:{consonant:"h"},post:[{vowel:"oo"}]},{pre:[{consonant:"k"}],main:{consonant:"c"},post:[{vowel:"e",coda:"n"},{consonant:"nz"}]}],[{pre:[],main:{consonant:"t"},post:[{vowel:"e",coda:"n"},{vowel:"e",coda:"ng"}]},{pre:[{consonant:"g"}],main:{consonant:"z"},post:[{vowel:"oo"},{vowel:"a",coda:"ng"}]}],[{pre:[{consonant:"x"}],main:{consonant:"d"},post:[{vowel:"e",coda:"ng"}]}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({
+              pre: [{ consonant: "y" }],
+              main: { consonant: "y" },
+              post: [{ vowel: "e", coda: "ng" }, { glide: "i" }],
+            }),
+            Sub({
+              pre: [{ consonant: "x" }],
+              main: { consonant: "h" },
+              post: [{ vowel: "oo" }],
+            }),
+            Sub({
+              pre: [{ consonant: "k" }],
+              main: { consonant: "c" },
+              post: [{ vowel: "e", coda: "n" }, { consonant: "nz" }],
+            }),
+          ],
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "t" },
+              post: [
+                { vowel: "e", coda: "n" },
+                { vowel: "e", coda: "ng" },
+              ],
+            }),
+            Sub({
+              pre: [{ consonant: "g" }],
+              main: { consonant: "z" },
+              post: [{ vowel: "oo" }, { vowel: "a", coda: "ng" }],
+            }),
+          ],
+          [
+            Sub({
+              pre: [{ consonant: "x" }],
+              main: { consonant: "d" },
+              post: [{ vowel: "e", coda: "ng" }],
+            }),
+          ],
+        ])
       })
 
       it("recognizes hyphens", function () {
         const source = "3-yruL"
         const result = compileShidinn(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{pre:[],main:{vowel:"e",coda:"n"},post:[]},{hyphen:true,pre:[{consonant:"y"}],main:{consonant:"r"},post:[{glide:"u",vowel:"a",coda:"u"}]}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({ pre: [], main: { vowel: "e", coda: "n" }, post: [] }),
+            Sub({
+              hyphen: true,
+              pre: [{ consonant: "y" }],
+              main: { consonant: "r" },
+              post: [{ glide: "u", vowel: "a", coda: "u" }],
+            }),
+          ],
+        ])
       })
 
       it("handles multiple prefixes correctly", function () {
         const source = "4oquV"
         const result = compileShidinn(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{pre:[{consonant:""},{vowel:"oo"}],main:{consonant:"q"},post:[{glide:"u",vowel:"e",coda:"i"}]}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({
+              pre: [{ consonant: "" }, { vowel: "oo" }],
+              main: { consonant: "q" },
+              post: [{ glide: "u", vowel: "e", coda: "i" }],
+            }),
+          ],
+        ])
       })
 
       it("handles vowelless characters correctly", function () {
         const source = "wup"
         const result = compileShidinn(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */ [[{pre:[],main:{consonant:"v"},post:[{glide:"u"},{consonant:"p"}]}]]
-        expect(result).to.deep.equal(expected)
+        expect(result).toEqual([
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "v" },
+              post: [{ glide: "u" }, { consonant: "p" }],
+            }),
+          ],
+        ])
       })
 
       it("should not treat i1, iE, u1, uA as valid finals", function () {
         const source = "4i1 4iE 4iA 4u1 4uE 4uA"
         const result = compileShidinn(source)
 
-        /** @type {CompiledText} */
-        const expected =
-          /* prettier-ignore */
+        expect(result).toEqual([
           [
-            [{pre:[],main:{consonant:""},post:[{glide:"i"},{}]}],
-            [{pre:[],main:{consonant:""},post:[{glide:"i"},{vowel:"ii"}]}],
-            [{pre:[],main:{consonant:""},post:[{glide:"i",vowel:"uu"}]}],
-            [{pre:[],main:{consonant:""},post:[{glide:"u"},{}]}],
-            [{pre:[],main:{consonant:""},post:[{glide:"u",vowel:"ii"}]}],
-            [{pre:[],main:{consonant:""},post:[{glide:"u"},{vowel:"uu"}]}]
-          ]
-        expect(result).to.deep.equal(expected)
+            Sub({
+              pre: [],
+              main: { consonant: "" },
+              post: [{ glide: "i" }, {}],
+            }),
+          ],
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "" },
+              post: [{ glide: "i" }, { vowel: "ii" }],
+            }),
+          ],
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "" },
+              post: [{ glide: "i", vowel: "uu" }],
+            }),
+          ],
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "" },
+              post: [{ glide: "u" }, {}],
+            }),
+          ],
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "" },
+              post: [{ glide: "u", vowel: "ii" }],
+            }),
+          ],
+          [
+            Sub({
+              pre: [],
+              main: { consonant: "" },
+              post: [{ glide: "u" }, { vowel: "uu" }],
+            }),
+          ],
+        ])
       })
     })
   })
--- test/draw.spec.js
+++ test/draw.spec.js
@@ -1,15 +1,69 @@
-const { expect } = require("chai")
+const { expect } = require("earl")
 const { draw } = require("../src/draw")
 
 describe("draw", function () {
   describe("draw()", function () {
     // This method returns an SVG so it's hard to write a spec for it, but
     it("should at least not write 'undefined' in its results", function () {
-      /** @type {import("../src/types").CompiledText} */
-      const ct =
-        /* prettier-ignore */ [[{main:{consonant:"n"},pre:[{glide:"i"}],post:[],reverseAffixes:true},{main:{consonant:"h"},pre:[{vowel:"a",coda:"u"}],post:[],reverseAffixes:true}],[{main:{consonant:"sh"},pre:[],post:[{}],reverseAffixes:true},{main:{consonant:"j"},pre:[],post:[{glide:"i",vowel:"e"}],reverseAffixes:true}],[{pre:[{vowel:"e",coda:"i"}],main:{consonant:"nj"},post:[{glide:"u",vowel:"e",coda:"ng"}]},{pre:[{vowel:"uu"}],main:{consonant:"nz"},post:[{vowel:"a",coda:"u"}]}],[{proper:true,pre:[{consonant:"x"}],main:{consonant:"d"},post:[{glide:"i",vowel:"e",coda:"ng"}]},{pre:[{vowel:"a"}],main:{consonant:"h"},post:[{vowel:"oo"}]}]]
-      const result = draw(ct)
-      expect(result).to.not.include("undefined")
+      const result = draw([
+        // ni3_hau3 sh4_jie4
+        [
+          {
+            main: { consonant: "n" },
+            pre: [{ glide: "i", reversed: true }],
+            post: [],
+            reverseAffixes: true,
+          },
+          {
+            main: { consonant: "h" },
+            pre: [{ vowel: "a", coda: "u", reversed: true }],
+            post: [],
+            reverseAffixes: true,
+          },
+        ],
+        [
+          {
+            main: { consonant: "sh" },
+            pre: [],
+            post: [{ reversed: true }],
+            reverseAffixes: true,
+          },
+          {
+            main: { consonant: "j" },
+            pre: [],
+            post: [{ glide: "i", vowel: "e", reversed: true }],
+            reverseAffixes: true,
+          },
+        ],
+
+        // Vnu8_AHL xdi8_aho
+        [
+          {
+            pre: [{ vowel: "e", coda: "i" }],
+            main: { consonant: "nj" },
+            post: [{ glide: "u", vowel: "e", coda: "ng" }],
+          },
+          {
+            pre: [{ vowel: "uu" }],
+            main: { consonant: "nz" },
+            post: [{ vowel: "a", coda: "u" }],
+          },
+        ],
+        [
+          {
+            proper: true,
+            pre: [{ consonant: "x" }],
+            main: { consonant: "d" },
+            post: [{ glide: "i", vowel: "e", coda: "ng" }],
+          },
+          {
+            pre: [{ vowel: "a" }],
+            main: { consonant: "h" },
+            post: [{ vowel: "oo" }],
+          },
+        ],
+      ])
+      expect(result).not.toInclude("undefined")
     })
   })
 })
